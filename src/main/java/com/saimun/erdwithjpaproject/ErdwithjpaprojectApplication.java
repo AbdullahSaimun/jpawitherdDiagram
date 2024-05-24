a39a463fa284a5a0d855ae77bdc4a5fc1cab8723
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-05-25 00:41:46 +0600

    init

---
+++ src/main/java/com/saimun/erdwithjpaproject/ErdwithjpaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.saimun.erdwithjpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdwithjpaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdwithjpaprojectApplication.class, args);
+	}
+
+}
