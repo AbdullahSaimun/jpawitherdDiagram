6774205064151f7f196002d14b182f282ac08643
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-05-25 17:33:45 +0600

    Ok

--- src/main/java/com/saimun/erdwithjpaproject/controller/AllController.java
+++ src/main/java/com/saimun/erdwithjpaproject/controller/AllController.java
@@ -1,9 +1,7 @@
 package com.saimun.erdwithjpaproject.controller;
 
-import com.saimun.erdwithjpaproject.model.Student;
-import com.saimun.erdwithjpaproject.model.Teacher;
-import com.saimun.erdwithjpaproject.service.StudentService;
-import com.saimun.erdwithjpaproject.service.TeacherService;
+import com.saimun.erdwithjpaproject.model.*;
+import com.saimun.erdwithjpaproject.service.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +12,47 @@ public class AllController {
 
 	private final TeacherService teacherService;
 	private final StudentService studentService;
+	private final MarkService markService;
+	private final SubjectService subjectService;
+	private final StudyGroupService studyGroupService;
+	private final SubjectTeacherService subjectTeacherService;
 
-	public AllController(TeacherService teacherService, StudentService studentService) {
+	public AllController(TeacherService teacherService, StudentService studentService, MarkService markService, SubjectService subjectService, StudyGroupService studyGroupService, SubjectTeacherService subjectTeacherService) {
 		this.teacherService = teacherService;
 		this.studentService = studentService;
+		this.markService = markService;
+		this.subjectService = subjectService;
+		this.studyGroupService = studyGroupService;
+		this.subjectTeacherService = subjectTeacherService;
 	}
 
 	@GetMapping(value = "/teacherList", produces = "application/json")
 	public List<Teacher> getTeacherList() {
 		return teacherService.getAllTeachers();
 	}
+
 	@GetMapping(value = "/studentList", produces = "application/json")
 	public List<Student> getStudentList() {
 		return studentService.getAllStudents();
+	}
+
+	@GetMapping(value = "/markList", produces = "application/json")
+	public List<Mark> getAllMarks() {
+		return markService.getAllMarks();
+	}
+
+
+	@GetMapping(value = "/subjectList", produces = "application/json")
+	public List<Subject> getAllSubject() {
+		return subjectService.getAllSubjects();
+	}
+
+	@GetMapping(value = "/studygroupList", produces = "application/json")
+	public List<StudyGroup> getAllStudyGroup() {
+		return studyGroupService.getAllGroups();
+	}
+	@GetMapping(value = "/subjectTeacherList", produces = "application/json")
+	public List<SubjectTeacher> getAllSubjectTeacher() {
+		return subjectTeacherService.getAllSubjectTeachers();
 	}
 }
